9527267734391e402757f1a585ff8bd860ec8385
guoliao <1923216474@qq.com>
2019-11-24 16:17:35 +0800

    fix:判断已登录情况下的页面状态

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -5,14 +5,33 @@
 
  const router=require('koa-router')()
 
+/**
+ * 获取登录信息
+ * @param {Oblect} ctx koa2 ctx
+ */
+ function getLoginInfo(ctx){
+     let data={
+         isLogin:false  //默认未登录
+     }
+     const userInfo=ctx.session.userInfo
+     if(userInfo){//登录了
+         data={
+             isLogin:true,
+             userName:userInfo.userName
+         }
+     }
+     return data
+ }
+
+
  //登录的路由
  router.get('/login',async (ctx,next)=>{
-     await ctx.render('login',{})
+     await ctx.render('login',getLoginInfo(ctx))
  })
 
  //注册的路由
  router.get('/register',async (ctx,next)=>{
-    await ctx.render('register',{})
+    await ctx.render('register',getLoginInfo(ctx))
 })
 
  module.exports=router
